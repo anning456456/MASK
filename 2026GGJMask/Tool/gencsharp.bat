f6e7e29727cf5f1d548c16f24ca6b44ec4cd33f4
srx <1057593034@qq.com>
2026-01-31 22:52:01 +0800

    修改配置工具文件路径

---
+++ 2026GGJMask/Tool/gencsharp.bat
@@ -0,0 +1,3 @@
+java -jar ../Tool/cfggen.jar -datadir ../../Excel/Config  -gen cs,dir:../Assets/Scripts/ConfigModule/,encoding:UTF-8 -gen bytes,file=../Config/config.bytes,cipher:
+
+pause
--- Tool/gencsharp.bat
+++
@@ -1,3 +0,0 @@
-java -jar ../Tool/cfggen.jar -datadir ../Excel/Config  -gen cs,dir:../Unity/Assets/Scripts/ConfigModule/,encoding:UTF-8 -gen bytes,file=../Unity/Config/config.bytes,cipher:
-
-pause
